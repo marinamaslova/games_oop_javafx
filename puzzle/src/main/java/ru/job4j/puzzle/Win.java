305258827d65de935edd38bddf1933da4049eaed
marinamaslova <marina_maslova_75@list.ru>
2021-05-24 15:49:10 +0300

    2. Сокобан. Тестовое задание. [#448890] v3

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -28,12 +28,11 @@ public class Win {
     public static boolean check(int[][] board) {
         boolean result = false;
         for (int index = 0; index < board.length; index++) {
-            if (board[index][index] == 1) {
-                if (monoHorizontal(board, index) || monoVertical(board, index)) {
+            if (board[index][index] == 1 && (monoHorizontal(board, index) || monoVertical(board, index)))  {
                     result = true;
                     break;
                 }
-            }
+
         }
         return result;
     }
